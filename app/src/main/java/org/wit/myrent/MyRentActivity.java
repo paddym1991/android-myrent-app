4a8e9b7b1eb98bf5d0efad5a92a29042a91b0d09
Paddym1991 <paddym1991.pm@gmail.com>
2017-09-29 14:39:28 +0100

    Handling listeners

--- app/src/main/java/org/wit/myrent/MyRentActivity.java
+++ app/src/main/java/org/wit/myrent/MyRentActivity.java
@@ -2,12 +2,40 @@ package org.wit.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
-public class MyRentActivity extends AppCompatActivity {
+public class MyRentActivity extends AppCompatActivity implements TextWatcher {
+
+    private EditText geolocation;
+    private Residence residence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myrent);
+
+        geolocation = (EditText) findViewById(R.id.geolocation);
+        residence = new Residence();
+
+        // Register a TextWatcher in the EditText geolocation object
+        geolocation.addTextChangedListener(this);
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable)
+    {
+        residence.setGeolocation(editable.toString());
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence c, int start, int count, int after)
+    {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence c, int start, int count, int after)
+    {
     }
 }
