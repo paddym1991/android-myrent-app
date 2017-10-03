ac8fc130cb8937440a3fa1c91ec361aeb3e1756c
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-03 22:12:23 +0100

    Implemented OnItemClickListener and associated intent to listen for clicks on the the residence list and carry out appropriate activity

--- app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
@@ -1,3 +1,6 @@
+//In order to update the list with the residence objects contained in the portfolio, we need an Adapter.
+//        An Adapter is a special class we can append to the end of the existing ResidenceListActivity class (make sure it is outside the closing brace of ResidenceListActivity).
+
 package org.wit.myrent.activities;
 
 import org.wit.myrent.R;
@@ -7,10 +10,12 @@ import org.wit.myrent.models.Residence;
 
 //Introducing ResidenceAdapter requires the following additional import statements
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
@@ -20,7 +25,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class ResidenceListActivity extends AppCompatActivity
+public class ResidenceListActivity extends Activity implements AdapterView.OnItemClickListener
 {
     private ListView listView;
     private Portfolio portfolio;
@@ -42,6 +47,16 @@ public class ResidenceListActivity extends AppCompatActivity
         //in order to trigger ResidenceAdapter
         adapter = new ResidenceAdapter(this, portfolio.residences);
         listView.setAdapter(adapter);
+
+        //install the ResidenceListActivity as a Listener
+        listView.setOnItemClickListener(this);
+    }
+
+    //interface method
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = new Intent(this, ResidenceActivity.class);
+        startActivity(intent);
     }
 }
 
