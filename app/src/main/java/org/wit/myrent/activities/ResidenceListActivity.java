bbede53b2103e71a613b544f0ffec3924890ee95
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-03 20:54:42 +0100

    Introduced an adapter to help update the list with the residence objects in the portfolio."

--- app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
@@ -3,14 +3,29 @@ package org.wit.myrent.activities;
 import org.wit.myrent.R;
 import org.wit.myrent.app.MyRentApp;
 import org.wit.myrent.models.Portfolio;
-import android.widget.ListView;
+import org.wit.myrent.models.Residence;
+
+//Introducing ResidenceAdapter requires the following additional import statements
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ResidenceListActivity extends AppCompatActivity
 {
     private ListView listView;
     private Portfolio portfolio;
+    //In order to incorporate it ResidenceAdapter into ResidenceListActivity, first introduce a new field
+    private ResidenceAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -23,5 +38,43 @@ public class ResidenceListActivity extends AppCompatActivity
 
         MyRentApp app = (MyRentApp) getApplication();
         portfolio = app.portfolio;
+
+        //in order to trigger ResidenceAdapter
+        adapter = new ResidenceAdapter(this, portfolio.residences);
+        listView.setAdapter(adapter);
+    }
+}
+
+
+class ResidenceAdapter extends ArrayAdapter<Residence>
+{
+    private Context context;
+
+    public ResidenceAdapter(Context context, ArrayList<Residence> residences)
+    {
+        super(context, 0, residences);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.list_item_residence, null);
+        }
+        Residence res = getItem(position);
+
+        TextView geolocation = (TextView) convertView.findViewById(R.id.residence_list_item_geolocation);
+        geolocation.setText(res.geolocation);
+
+        TextView dateTextView = (TextView) convertView.findViewById(R.id.residence_list_item_dateTextView);
+        dateTextView.setText(res.getDateString());
+
+        CheckBox rentedCheckBox = (CheckBox) convertView.findViewById(R.id.residence_list_item_isrented);
+        rentedCheckBox.setChecked(res.rented);
+
+        return convertView;
     }
 }
