c0783e1620ceaf70ee1879ea3a7cfec00c7bea46
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-04 11:38:12 +0100

    portfolio class will now be equipped with the capability to use the serializer to save | restore the Residences it is managing.

--- app/src/main/java/org/wit/myrent/models/Portfolio.java
+++ app/src/main/java/org/wit/myrent/models/Portfolio.java
@@ -3,14 +3,30 @@ import java.util.ArrayList;
 
 import android.util.Log;
 
+import static org.wit.android.helpers.LogHelpers.info;
+
 public class Portfolio
 {
     public ArrayList<Residence> residences;
+    // introduce the serializer as a member of the Portfolio class
+    private PortfolioSerializer   serializer;
 
-    public Portfolio() {
-        residences = new ArrayList<Residence>();
+    //revise the constructor to take a serializer when it is being initialised
+    public Portfolio(PortfolioSerializer serializer)
+    {
+        this.serializer = serializer;
+        try
+        {
+            residences = serializer.loadResidences();
+        }
+        catch (Exception e)
+        {
+            info(this, "Error loading residences: " + e.getMessage());
+            residences = new ArrayList<Residence>();
+        }
     }
 
+    //method to add a residence to the list
     public void addResidence(Residence residence) {
         residences.add(residence);
     }
@@ -24,5 +40,21 @@ public class Portfolio
             }
         }
         return null;
+    }
+
+    //introduce a new method to save all the residences to disk
+    public boolean saveResidences()
+    {
+        try
+        {
+            serializer.saveResidences(residences);
+            info(this, "Residences saved to file");
+            return true;
+        }
+        catch (Exception e)
+        {
+            info(this, "Error saving residences: " + e.getMessage());
+            return false;
+        }
     }
 }
