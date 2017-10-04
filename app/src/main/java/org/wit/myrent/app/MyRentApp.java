6b1c3ff41a0a095ba73b2cd88b1844c25059ee80
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-04 11:44:55 +0100

    Application object (MyRentApp) updated to implement Serializer and JSON

--- app/src/main/java/org/wit/myrent/app/MyRentApp.java
+++ app/src/main/java/org/wit/myrent/app/MyRentApp.java
@@ -1,18 +1,24 @@
 package org.wit.myrent.app;
 
 import org.wit.myrent.models.Portfolio;
+import org.wit.myrent.models.PortfolioSerializer;
+
 import android.app.Application;
 import static org.wit.android.helpers.LogHelpers.info;
 
 public class MyRentApp extends Application
 {
     public Portfolio portfolio;
+    //introduce a field to hold the file name we will use to store the portfolio
+    private static final String FILENAME = "portfolio.json";
 
     @Override
     public void onCreate()
     {
         super.onCreate();
-        portfolio = new Portfolio();
+        //portfolio creation
+        PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+        portfolio = new Portfolio(serializer);
 
         info(this, "MyRent app is launched");
     }
