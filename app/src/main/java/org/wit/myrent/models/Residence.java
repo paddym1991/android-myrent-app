a470b43e338c48ec92366c541edeeeb45f91f0e9
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-04 11:23:30 +0100

    ability to save and restore residence model to some external format. In this case JSON.

--- app/src/main/java/org/wit/myrent/models/Residence.java
+++ app/src/main/java/org/wit/myrent/models/Residence.java
@@ -3,6 +3,9 @@ package org.wit.myrent.models;
 import java.util.Date;
 import java.util.Random;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Residence {
     public Long id;
     public Long date;
@@ -11,6 +14,12 @@ public class Residence {
     //example "52.4566,-6.5444"
     public String geolocation;
     public boolean rented;
+
+    //define in our classes appropriate names for each of the fields we wish to serialize
+    private static final String JSON_ID             = "id"            ;
+    private static final String JSON_GEOLOCATION    = "geolocation"   ;
+    private static final String JSON_DATE           = "date"          ;
+    private static final String JSON_RENTED         = "rented"        ;
 
 
     public Residence() {
@@ -30,6 +39,26 @@ public class Residence {
             rndVal = new Random().nextLong();
         } while (rndVal <= 0);
         return rndVal;
+    }
+
+    //new constructor to load a Residence object from JSON
+    public Residence(JSONObject json) throws JSONException
+    {
+        id            = json.getLong(JSON_ID);
+        geolocation   = json.getString(JSON_GEOLOCATION);
+        date          = json.getLong(JSON_DATE);
+        rented        = json.getBoolean(JSON_RENTED);
+    }
+
+    //method to save an object to JSON
+    public JSONObject toJSON() throws JSONException
+    {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID            , Long.toString(id));
+        json.put(JSON_GEOLOCATION   , geolocation);
+        json.put(JSON_DATE          , date);
+        json.put(JSON_RENTED        , rented);
+        return json;
     }
 
     public void setGeolocation(String geolocation) {
