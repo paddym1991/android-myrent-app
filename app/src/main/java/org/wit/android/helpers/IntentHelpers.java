ca556b3b769887b62549dd2b1ddf76d09ed42247
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-04 10:11:49 +0100

    added new helper class 'IntentHelpers'.

---
+++ app/src/main/java/org/wit/android/helpers/IntentHelpers.java
@@ -0,0 +1,28 @@
+package org.wit.android.helpers;
+import java.io.Serializable;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class IntentHelpers
+{
+    public static void startActivity (Activity parent, Class classname)
+    {
+        Intent intent = new Intent(parent, classname);
+        parent.startActivity(intent);
+    }
+
+    public static void startActivityWithData (Activity parent, Class classname, String extraID, Serializable extraData)
+    {
+        Intent intent = new Intent(parent, classname);
+        intent.putExtra(extraID, extraData);
+        parent.startActivity(intent);
+    }
+
+    public static void startActivityWithDataForResult (Activity parent, Class classname, String extraID, Serializable extraData, int idForResult)
+    {
+        Intent intent = new Intent(parent, classname);
+        intent.putExtra(extraID, extraData);
+        parent.startActivityForResult(intent, idForResult);
+    }
+}
