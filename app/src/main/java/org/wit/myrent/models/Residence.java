4100d3764e306b0d44e2ec8a79b1332f6f025894
Paddym1991 <paddym1991.pm@gmail.com>
2017-10-06 15:27:48 +0100

    Extending the Residence model class to include a new field(the tenant) and also a new method, one to generate a report.

--- app/src/main/java/org/wit/myrent/models/Residence.java
+++ app/src/main/java/org/wit/myrent/models/Residence.java
@@ -3,12 +3,16 @@ package org.wit.myrent.models;
 import java.util.Date;
 import java.util.Random;
 
+import org.wit.myrent.R;
+import android.content.Context;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Residence {
     public Long id;
     public Long date;
+    public String tenant;
 
     //a latitude longitude pair
     //example "52.4566,-6.5444"
@@ -20,12 +24,14 @@ public class Residence {
     private static final String JSON_GEOLOCATION    = "geolocation"   ;
     private static final String JSON_DATE           = "date"          ;
     private static final String JSON_RENTED         = "rented"        ;
+    private static final String JSON_TENANT         = "tenant"        ;
 
 
     public Residence() {
         id = unsignedLong();
         date = new Date().getTime();
         geolocation = "52.253456,-7.187162";
+        tenant = ": none presently";
     }
 
     /**
@@ -48,9 +54,10 @@ public class Residence {
         geolocation   = json.getString(JSON_GEOLOCATION);
         date          = json.getLong(JSON_DATE);
         rented        = json.getBoolean(JSON_RENTED);
+        tenant        = json.getString(JSON_TENANT);
     }
 
-    //method to save an object to JSON
+    //method to save an object to JSON (serialisation)
     public JSONObject toJSON() throws JSONException
     {
         JSONObject json = new JSONObject();
@@ -58,6 +65,7 @@ public class Residence {
         json.put(JSON_GEOLOCATION   , geolocation);
         json.put(JSON_DATE          , date);
         json.put(JSON_RENTED        , rented);
+        json.put(JSON_TENANT        , tenant);
         return json;
     }
 
@@ -76,5 +84,26 @@ public class Residence {
     private String dateString() {
         String dateFormat = "EEE d MMM yyyy H:mm";
         return android.text.format.DateFormat.format(dateFormat, date).toString();
+    }
+
+    public String getResidenceReport(Context context) {
+        String rentedString = "";
+        if (rented) {
+            rentedString = context.getString(R.string.residence_report_rented);
+        }
+        else {
+            rentedString = context.getString(R.string.residence_report_not_rented);
+        }
+
+        String prospectiveTenant = tenant;
+        if (tenant == null) {
+            prospectiveTenant = context.getString(R.string.residence_report_nobody_interested);
+        }
+        else {
+            prospectiveTenant = context.getString(R.string.residence_report_prospective_tenant, tenant);
+        }
+        String report = "Location " + geolocation + " Date: " + dateString() + " " + rentedString + " " + prospectiveTenant;
+        return report;
+
     }
 }
